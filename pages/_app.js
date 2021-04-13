f105bccb982ca6349a0aabd6888d7f8546660ec1
Nico Albanese <gcalbanese96@gmail.com>
2021-04-13 19:21:46 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,52 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import NextLink from "next/link";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+  html, body {
+	margin: 0;
+	font-family: sans-serif;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+h1, h2, h3, h4, h5, p {
+	margin: 0;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url("fonts/Montserrat/Montserrat-Bold.ttf");
+  font-weight: 800;
+}
+@font-face {
+  font-family: "Montserrat";
+  src: url("fonts/Montserrat/Montserrat-Regular.ttf");
+  font-weight: 500;
+}
+
+h1, h2, h3, h4, h5 {
+  font-family: "Montserrat";
+}
+
+p {
+  font-family: Avenir, sans-serif;
+}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primaryOne: "#2d2d41",
+    primaryTwo: "#f4ebe5",
+    primaryThree: "#9da6fc",
+    secondaryOne: "#a8a3c6",
+    secondaryTwo: "#ddabbb",
+    secondaryThree: "#f2e2ff",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +56,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
