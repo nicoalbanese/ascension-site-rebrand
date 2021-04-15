d21347ec7ea9aba89733a05c3e46c8749f469cda
Nico Albanese <gcalbanese96@gmail.com>
2021-04-15 23:02:14 +0100

    got static pages working, potential font issue

--- pages/portfolio/[company].js
+++ pages/portfolio/[company].js
@@ -1,8 +1,64 @@
-import React from "react";
+import Layout from "../../components/Layout";
+import { getPortfolio } from "../../lib/airtable";
 
-const companyDetailed = () => {
-  return <div>Hello</div>;
+import Image from "next/image";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  height: 100%;
+  width: 100%;
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  p {
+    margin-top: 1rem;
+  }
+
+  .main-container {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: 1fr 2fr;
+  }
+`;
+
+const companyDetailed = ({ company }) => {
+  console.log(company);
+  return (
+    <Layout>
+      <Wrapper>
+        <div className='main-container'>
+          <Image src={company.logoUrl} height={300} width={300} />
+          <div>
+            <h1>{company.name}</h1>
+            <p>{company.description}</p>
+          </div>
+        </div>
+      </Wrapper>
+    </Layout>
+  );
 };
 
 export default companyDetailed;
 
+export async function getStaticPaths() {
+  const companies = await getPortfolio();
+
+  const paths = companies.map((company) => {
+    return {
+      params: { company: company.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const companies = await getPortfolio();
+  const [company] = companies.filter((c) => c.slug === params.company);
+  return { props: { company } };
+}
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,15 +1,16 @@
 import Layout from "../../components/Layout";
-import head from "next/head";
+import Head from "next/head";
 
 import PortfolioDetailed from "../../components/PortfolioDetailed";
+import { getPortfolio } from "../../lib/airtable";
 
 const portfolio = ({ companies }) => {
   //   console.log(companies, moreData);
   return (
     <Layout>
-      <head>
+      <Head>
         <title>Portfolio</title>
-      </head>
+      </Head>
       <div className='description'>
         <h1>Portfolio</h1>
         <p>
@@ -30,58 +31,8 @@ const portfolio = ({ companies }) => {
 export default portfolio;
 
 export async function getStaticProps() {
-  let myHeaders = new Headers();
-  myHeaders.append(
-    "Authorization",
-    `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_KEY}`
-  );
-  myHeaders.append("Cookie", "brw=brwy1TrDiZyNAsU5u");
-
-  let requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-  const AIRTABLE_URL =
-    "https://api.airtable.com/v0/appm10v13QJCjL2PL/Website%20Portfolio?view=PortfolioAPI";
-  const res = await fetch(AIRTABLE_URL, requestOptions);
-  const data = await res.json();
-
-  let structuredData = data.records.map((company) => {
-    return {
-      name: company.fields.Name,
-      url: company.fields.Website,
-      slug: company.fields["[website] slug"],
-      description: company.fields.Description,
-      logoUrl: company.fields.Logo[0].url,
-      id: company.id,
-      category: company.fields.Category,
-    };
-  });
-
-  let moreData;
-  if (data.offset) {
-    const secondRes = await fetch(
-      `${AIRTABLE_URL}&offset=${data.offset}`,
-      requestOptions
-    );
-    const secondData = await secondRes.json();
-    moreData = secondData.records.map((company) => {
-      return {
-        name: company.fields.Name,
-        url: company.fields.Website,
-        slug: company.fields["[website] slug"],
-        description: company.fields.Description,
-        logoUrl: company.fields.Logo[0].url,
-        id: company.id,
-        category: company.fields.Category,
-      };
-    });
-  }
-  structuredData = [...structuredData, ...moreData];
-
   //   console.log(data);
-
+  const structuredData = await getPortfolio();
   return {
     props: {
       companies: structuredData,
