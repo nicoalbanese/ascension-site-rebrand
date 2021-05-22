8b1762719c0006549874879472bc29bb8ea1cfd2
Nico Albanese <gcalbanese96@gmail.com>
2021-05-22 11:09:15 +0100

    more layout animations

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,29 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
 import styled from "styled-components";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
-const InnerWrapper = styled.div`
+const InnerWrapper = styled(motion.div)`
   width: 70%;
   max-width: 1024px;
   margin: auto;
   padding: 2rem;
 `;
 
-const OuterWrapper = styled.div`
-  width: 100%;
-`;
+const pageVariants = {
+  initial: {
+    opacity: 0,
+  },
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
 
 const ChildrenContainer = styled.div`
   margin-top: 3rem;
@@ -22,7 +32,13 @@ const ChildrenContainer = styled.div`
 const Layout = ({ children }) => {
   return (
     <>
-      <InnerWrapper className='inner'>
+      <InnerWrapper
+        className='inner'
+        initial='initial'
+        animate='in'
+        exit='out'
+        variants={pageVariants}
+      >
         <NavBar />
         <ChildrenContainer>{children}</ChildrenContainer>
       </InnerWrapper>
