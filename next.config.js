f1ef5b3d8ab58f037459c66a259d2156b33c1dde
Nico Albanese <gcalbanese96@gmail.com>
2021-06-28 15:49:57 +0100

    added mainspring redirect on apply endpoint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    images: {
-      domains: ['dl.airtable.com'],
-    },
-  }
+  images: {
+    domains: ["dl.airtable.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/apply",
+        destination: "https://apps.mainspringfs.com/forms/ascension/",
+        permanent: true,
+      },
+    ];
+  },
+};
