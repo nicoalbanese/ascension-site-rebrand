3ab79898cc9fcdfc461d650ef101502e4772b125
Nico Albanese <gcalbanese96@gmail.com>
2022-04-08 17:52:56 +0100

    trying different way

--- pages/investors/[fund].js
+++ pages/investors/[fund].js
@@ -176,7 +176,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 }
 
--- pages/portfolio/[company].js
+++ pages/portfolio/[company].js
@@ -222,7 +222,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
